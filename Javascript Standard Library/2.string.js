be492a82f829ae4d2772c0d452fe6d1d11f2be6f
faqihfnf <faqih.fnf@gmail.com>
2025-04-08 21:54:27 +0700

    string

---
+++ Javascript Standard Library/2.string.js
@@ -0,0 +1,10 @@
+const nama = "Faqih Nur Fahmi";
+
+console.log(nama.length); // 15
+console.log(nama.toUpperCase()); // FAQIH NUR FAHMI
+console.log(nama.toLowerCase()); // faqih nur fahmi
+console.log(nama.split(" ")); // [ 'Faqih', 'Nur', 'Fahmi' ]
+
+const input = "                input             data";
+console.log(input.trim());
+console.log(input);
