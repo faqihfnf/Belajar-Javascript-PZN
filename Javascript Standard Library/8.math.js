321386f8b5390e18f1c7df800cf3cbdd4f076c32
faqihfnf <faqih.fnf@gmail.com>
2025-07-07 00:13:06 +0700

    math

---
+++ Javascript Standard Library/8.math.js
@@ -0,0 +1,8 @@
+console.log(Math.PI); // 3.141592653589793
+console.log(Math.E); // 2.718281828459045
+console.log(Math.max(1, 2, 3, 4, 5)); // 5
+console.log(Math.min(1, 2, 3, 4, 5)); // 1
+console.log(Math.round(1.4)); // 1
+console.log(Math.round(1.8)); // 2
+console.log(Math.ceil(1.4)); // 2
+console.log(Math.floor(1.4)); // 1
